a6ddd357e030b6c2e4fdee1224755f36f776c85e
AGL-12” <aglgg21@gmail.com>
2025-03-21 09:27:47 +0100

    Rama final-comprobar modelo

--- src/modelo/Articulo.java
+++ src/modelo/Articulo.java
@@ -1,5 +1,74 @@
 package modelo;
 
 public class Articulo {
+	private int id_art;
+	private String nombre;
+	private String descripcion;
+	private int stock;
+	private float precio;
+	private float oferta;
+	private Seccion seccion;
+
+	public int getId_art() {
+		return id_art;
+	}
+
+	public void setId_art(int id_art) {
+		this.id_art = id_art;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public float getOferta() {
+		return oferta;
+	}
+
+	public void setOferta(float oferta) {
+		this.oferta = oferta;
+	}
+
+	public Seccion getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(Seccion seccion) {
+		this.seccion = seccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Articulo [id_art=" + id_art + ", nombre=" + nombre + ", descripcion=" + descripcion + ", stock=" + stock
+				+ ", precio=" + precio + ", oferta=" + oferta + ", seccion=" + seccion + "]";
+	}
 
 }
--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -1,5 +1,49 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class Pedido {
+	private int id_ped;
+	private int id_usu;
+	private float total;
+	private LocalDateTime fecha_compra;
+
+	public int getId_ped() {
+		return id_ped;
+	}
+
+	public void setId_ped(int id_ped) {
+		this.id_ped = id_ped;
+	}
+
+	public int getId_usu() {
+		return id_usu;
+	}
+
+	public void setId_usu(int id_usu) {
+		this.id_usu = id_usu;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public LocalDateTime getFecha_compra() {
+		return fecha_compra;
+	}
+
+	public void setFecha_compra(LocalDateTime fecha_compra) {
+		this.fecha_compra = fecha_compra;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id_ped=" + id_ped + ", id_usu=" + id_usu + ", total=" + total + ", fecha_compra=" + fecha_compra
+				+ "]";
+	}
 
 }
