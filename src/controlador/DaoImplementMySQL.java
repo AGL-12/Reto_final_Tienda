a5ddec42100a0e9efd0f9231d67e93186173c543
mikel <mikel@GORGOROTH>
2025-03-22 13:15:48 +0100

    Login funciona

--- src/controlador/DaoImplementMySQL.java
+++ src/controlador/DaoImplementMySQL.java
@@ -22,27 +22,31 @@ public class DaoImplementMySQL implements Dao {
 	private Connection con;
 	private PreparedStatement stmt;
 	// Sentencias SQL
-	final String LOGIN = "select * from usuario where usuario=? and contra=?";
+	final String LOGIN = "SELECT * FROM cliente WHERE usuario = ? AND contra = ?";
 	final String ALTAPROP = "insert into propietario (nombre,fechaNace) values (?,?)";
 
-	public DaoImplementMySQL() {
+	/*public DaoImplementMySQL() {
 		this.configFile = ResourceBundle.getBundle("modelo.configClase");
 		this.urlBD = this.configFile.getString("Conn");
 		this.userBD = this.configFile.getString("DBUser");
 		this.passwordBD = this.configFile.getString("DBPass");
-	}
+	}*/
 
 	private void openConnection() {
 		try {
-			con = DriverManager.getConnection(urlBD, this.userBD, this.passwordBD);
+		//	con = DriverManager.getConnection(urlBD, this.userBD, this.passwordBD);
 
-			/*
-			 * DriverManager.getConnection(
-			 * "jdbc:mysql://localhost:3306/tienda_brico?serverTimezone=Europe/Madrid&useSSL=false",
-			 * "root", "abcd*1234");
-			 */
+			
+			/*con =  DriverManager.getConnection(
+			  "jdbc:mysql://localhost:3306/tienda_brico?serverTimezone=Europe/Madrid&useSSL=false",
+			  "root", "abcd*1234");*/
+			//con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda_brico?serverTimezone=Europe/Madrid&useSSL=false", "root", "abcd*1234");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda_brico?serverTimezone=Europe/Madrid&useSSL=false", "root", "abcd*1234");
+
+
+			 
 		} catch (SQLException e) {
-			System.out.println("Error al intentar abrir la BD");
+			System.out.println("Error al intentar abrir la BD" +e.getMessage());
 		}
 	}
 
