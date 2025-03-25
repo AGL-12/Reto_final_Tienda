def546c2fae12d78a7bc83a7e5aeda9dc41d54b2
precursorvt <precursorvt@gmail.com>
2025-03-25 13:53:17 +0100

    Adaptacion a mi base de datos

--- src/controlador/DaoImplementMySQL.java
+++ src/controlador/DaoImplementMySQL.java
@@ -22,7 +22,7 @@ public class DaoImplementMySQL implements Dao {
 	private Connection con;
 	private PreparedStatement stmt;
 	// Sentencias SQL
-	final String LOGIN = "select * from usuario where usuario=? and contra=?";
+	final String LOGIN = "select * from cliente where usuario=? and contra=?";
 	final String ALTAPROP = "insert into propietario (nombre,fechaNace) values (?,?)";
 
 	public DaoImplementMySQL() {
@@ -61,7 +61,6 @@ public class DaoImplementMySQL implements Dao {
 		openConnection();
 		try {
 			stmt = con.prepareStatement(LOGIN);
-
 			stmt.setString(1, cli.getUsuario());
 			stmt.setString(2, cli.getContra());
 
